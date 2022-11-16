944b18e9113c102c7d2ed395640b00f895ff2bef
soulfulart-V1 <paulo_workstation@hotmail.com>
2022-11-16 15:48:54 -0300

    update lambda function for project 210904307

--- ENGINEERIOT/Temperature Machine One/PIPELINE_ENGINEERINGIOT_210904307_IOTGetData/LAMBDA/EXTRACT_ENGINEERINGIOT_210904307_GETIoTData/lambda_function.py
+++ ENGINEERIOT/Temperature Machine One/PIPELINE_ENGINEERINGIOT_210904307_IOTGetData/LAMBDA/EXTRACT_ENGINEERINGIOT_210904307_GETIoTData/lambda_function.py
@@ -1,13 +1,18 @@
 import os
 import json
+import boto3
 import requests
+import datetime
 
+from pandas import DataFrame
+
+#global itens
+bucket_name = 'soulfulart-data-lake'
 
 def lambda_handler(event, context):
 
     headers = {
-        'Kafka-Auto-Offset-Reset': 'earliest',
-        
+        'Kafka-Auto-Offset-Reset': 'earliest'   
     }
     
     response = requests.get(
@@ -16,10 +21,27 @@ def lambda_handler(event, context):
         auth=(os.environ['SASL_PLAIN_USERNAME'], os.environ['SASL_PLAIN_PASSWORD'])
         )
         
-    messages = response.text
+    messages = json.loads(response.text.replace("'", '"'))
+
+    device_data_field = 'physical_data'
+    messages_physical_data = []
+
+    for message in messages:
+        message_values = message["value"].replace("'", '"')
+        message_values = message_values.replace("\\t", '')
+        message_values = message_values.replace("\\n", '')
+        message_values = json.loads(message_values)
+        messages_physical_data.append(message_values[device_data_field])
+
+    final_dataframe = DataFrame.from_records(messages_physical_data)
+
+    object_path = 's3://'+bucket_name+'/ENGINEERINGIOT/IOT_GET_DATA_210904307/STREAM_DATA/PHYSICAL_VALUES'
+    file_name = str(datetime.datetime.now())
+    object_path = object_path + file_name +'.parquet.gzip'
+    final_dataframe.to_parquet(object_path, compression='gzip')
     
     return {
 
-        'message' : str(messages)
+        'message' : 'File'+object_path+' write succesfully!'
         
     }
