e44c16b12a0b5df35a778d5c6b9d4102d8ad31fa
soulfulart-V1 <paulo_workstation@hotmail.com>
2022-11-25 11:39:48 -0300

    updated related to project IOT_GET_DATA_210904307

--- ENGINEERIOT/Temperature Machine One/PIPELINE_ENGINEERINGIOT_210904307_IOTGetData/GLUE/LOAD_ENGINEERINGIOT_210904307_GETIoTData_BatchData/script.py
+++ ENGINEERIOT/Temperature Machine One/PIPELINE_ENGINEERINGIOT_210904307_IOTGetData/GLUE/LOAD_ENGINEERINGIOT_210904307_GETIoTData_BatchData/script.py
@@ -7,10 +7,10 @@ from pyspark.sql import functions as F
 from awsglue.utils import getResolvedOptions
 
 ##aws services
-s3 = boto3.client('s3')
+s3_client = boto3.client('s3')
 
 ## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'run_job_date'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'run_job_date', 'bucket_name'])
 
 year = int(args['run_job_date'][0:4])
 month = int(args['run_job_date'][5:7])
@@ -35,11 +35,12 @@ if (today_date_minus_one.day<10):
 else: used_day = str(today_date_minus_one.day)
 
 #input parameters
-bucket_name = 'soulfulart-data-lake'
+bucket_name = args['bucket_name']
 partition_column = 'yearmonthday'
 partition_value = used_year+used_month+used_day
 input_root_path = 's3://'+bucket_name+'/STREAM/ENGINEERINGIOT/IOT_GET_DATA_210904307/REGION_A/PHYSICAL_VALUES/'
 input_file_path =  input_root_path + partition_column+'='+partition_value+'/'
+input_file_path_delete = input_root_path.replace('s3://'+bucket_name+'/','')
 output_file_path = input_file_path.replace('STREAM', 'RAW')
 output_file_path = output_file_path[:-1]
 
@@ -53,4 +54,10 @@ df = df.coalesce(1)
 df.write.option("header", "true").mode("overwrite").parquet(output_file_path)
 
 #delete stream files
-s3.delete_object(Bucket=bucket_name, Key=input_file_path)
+filter_client = s3_client.list_objects(Bucket=bucket_name, Prefix=input_file_path_delete)
+
+keys_deleted=[]
+
+for item in filter_client['Contents']:
+    s3_client.delete_object(Bucket=bucket_name, Key=item['Key'])
+    keys_deleted.append(item['Key'])
